0adc8a4623cb776f649047823a94f88751781e77
akram <dh.akram@gmail.com>
2025-02-22 20:56:42 +0100

    mise a jour script

--- cybersec_gpt.py
+++ cybersec_gpt.py
@@ -5,8 +5,7 @@ import os
 API_KEY = os.getenv("OPENAI_API_KEY")
 
 # 📌 ID du GPT personnalisé (à récupérer sur OpenAI)
-GPT_ID = "g-67b9896d3d3881918073bac8dc5faa91-cybersec-watcher"  # Remplace par ton ID exact
-#https://chatgpt.com/g/g-67b9896d3d3881918073bac8dc5faa91-cybersec-watcher
+GPT_ID = "g-67b9896d3d3881918073bac8dc5faa91"  # Remplace par ton ID exact
 # ✅ Initialiser le client OpenAI
 client = openai.OpenAI(api_key=API_KEY)
 
